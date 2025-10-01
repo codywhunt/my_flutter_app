9329f2001ad0389aad1622d1081a8bff53494534
Cody <cody@flutterflow.io>
2025-10-01 19:38:58 -0400

    Update appbar title in main.dart to 'hello world FROM GITHUB'

--- lib/main.dart
+++ lib/main.dart
@@ -83,7 +83,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text('hello world'),
+        title: Text('hello world FROM GITHUB'),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
